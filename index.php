b88f45846c9ba4f25f24f9d8ef1628e4462865db
Manuell00 <manuell.caselli@gmail.com>
2023-06-30 15:41:12 +0200

    Inserimento attributi e collegamento parametri

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<!-- Creo una classe : Movie -->
+<?php
 
-class Movie {
+// Creo una classe : Movie
+class Movie
+{
+    public $name;
+    public $author;
+    public $ticket_price;
 
+    //   Utilizzo la funzione __construct da richiamare con new successivamente
+    public function __construct($name, $author, $ticket_price)
+    {
+
+        // Assegnamo il valore del parametro $name all'attributo $name dell'oggetto corrente.
+        // Facciamo lo stesso con i restanti attributi
+        $this->name = $name;
+        $this->author = $author;
+        $this->ticket_price = $ticket_price;
+    }
 }
 
-$movie_1 = new Movie()
-$movie_2 = new Movie()
+$movie_1 = new Movie("Avatar", "James Cameron", "10$");
+var_dump($movie_1);
